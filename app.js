60552afeac4fd30ff8c6e7bdbb3f893ac33c92fe
Charlie Fitzgerald <46912450+charlie-fitzgerald@users.noreply.github.com>
2023-08-17 16:42:31 -0700

    Make input functional. Add reminders after submit

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-console.log("Connected")
+const reminderSubmitBtn = document.querySelector("#reminder-submit-btn");
+const reminderInput = document.querySelector("#reminder-input");
+const reminderList = document.querySelector("#reminder-list");
+
+const reminders = [];
+
+function addNewReminder(input) {
+    reminders.push(input.value);
+}
+
+function createReminder(item) {
+    const li = document.createElement('li');
+    li.innerText = item;
+
+    return li
+}
+
+function displayReminders(arr) {
+
+    for (let i = 0; i < arr.length; i++) {
+        let reminder = createReminder(arr[i]);
+
+        reminderList.appendChild(reminder);
+    }
+
+}
+
+reminderSubmitBtn.addEventListener("click", e => {
+    e.preventDefault();
+
+    
+    reminderList.innerHTML = '';
+
+    addNewReminder(reminderInput);
+    displayReminders(reminders);
+    reminderInput.value = '';
+});
+
+reminderInput.addEventListener("keypress", e => {   
+
+    if (e.key === "Enter") {
+        reminderSubmitBtn.click();
+    }
+})
